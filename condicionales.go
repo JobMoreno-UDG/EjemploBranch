92a64d8c84aa2d47807f79aef404f49aaec71cc1
JobMoreno-UDG <job.moreno7426@alumnos.udg.mx>
2020-09-28 20:48:03 -0500

    ejemplo Branch

---
+++ condicionales.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var temp int
+	fmt.Print("Temp: ")
+	fmt.Scan(&temp)
+	if temp>0{
+		fmt.Println("Temperatura Positiva")
+	}else if temp ==0 {
+		fmt.Print("temperatura cero")
+	}else {
+		fmt.Println("Temperatura bajo cero")
+	}
+}
